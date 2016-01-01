a739737291932fb19f4fc2c1bf1ef38337b005b0
Andrea Wetzel <wetzelcreative@gmail.com>
2016-01-01 11:02:53 -0600

    needed to run gulp to fix error

--- js/app.js
+++ js/app.js
@@ -1,7 +1,5 @@
 document.querySelector('#js-content').innerHTML += 'Here is some text';
 
-var foo = bar;
-
 document.querySelector('#js-content').innerHTML += ' and more text from two';
 
 //# sourceMappingURL=app.js.map
